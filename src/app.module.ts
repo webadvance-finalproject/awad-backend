ae977e32d09a9d174137273ea7f08bb7058356a6
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2024-12-09 00:29:21 +0700

    feat: Implement get detail movie

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { CommonModule } from './modules/common/common.module';
+import { MovieModule } from './modules/movie/movie.module';
 @Module({
-  imports: [AuthModule, UserModule, CommonModule],
+  imports: [AuthModule, UserModule, CommonModule, MovieModule],
 })
 export class AppModule {}
