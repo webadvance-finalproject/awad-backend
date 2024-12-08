fe8787be8c41a286b79d512de091c509740a1579
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2024-12-09 01:17:24 +0700

    feat: Enable CORS for BE

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const API_DEFAULT_PORT = 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.API_PORT || API_DEFAULT_PORT);
 }
 bootstrap();
