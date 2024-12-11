c3d88705f9d1d07e209d9c702b11ec61406de5c6
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2024-12-11 18:43:16 +0700

    feat(homepage): api for homepage movie list

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const API_DEFAULT_PORT = 3000;
--- src/modules/movie/utils/external-api.util.ts
+++ src/modules/movie/utils/external-api.util.ts
@@ -1,6 +1,44 @@
 import axios from 'axios';
 import { GetMovieDetailDto } from '../dto';
 
+export async function fetchTrendingMoviesByDayFromExternalAPI(options: {
+  token: string;
+  page: number;
+}) {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?page=${options.page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${options.token}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function fetchTrendingMoviesByWeekFromExternalAPI(options: {
+  token: string;
+  page: number;
+}) {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/week?page=${options.page}`,
+      {
+        headers: {
+          Authorization: `Bearer ${options.token}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
 export async function fetchMovieFromExternalAPI(options: {
   token: string;
   movieID: GetMovieDetailDto;
