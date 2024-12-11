36846c006f16e452c6805a7d8ed87d15964859b9
21120538 <luuvinhquangqg@gmail.com>
2024-12-11 21:26:57 +0700

    feat(search):  search movie

--- src/modules/movie/movie.controller.ts
+++ src/modules/movie/movie.controller.ts
@@ -9,6 +9,7 @@ import {
   Param,
   HttpException,
   Query,
+  ParseIntPipe
 } from '@nestjs/common';
 import { GetMovieDetailDto } from './dto';
 import { MovieService } from './movie.service';
@@ -17,6 +18,17 @@ import { AuthFirebaseGuard } from '../common/guards';
 @Controller('movie')
 export class MovieController {
   constructor(private movieService: MovieService) {}
+
+  @Get('search')
+  // @UseGuards(AuthFirebaseGuard)
+  @HttpCode(HttpStatus.OK)
+  async searchMovie(@Query('keyword') keyword: string, @Query('page', ParseIntPipe) page: number = 1) {
+    try {
+      return await this.movieService.searchMovie(keyword, page);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
   @Get(':id')
   @UseGuards(AuthFirebaseGuard)
