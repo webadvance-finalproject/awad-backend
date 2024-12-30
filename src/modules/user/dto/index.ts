707bcc7a263e13851dfb8c4109c36d364184529e
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2024-12-30 16:03:19 +0700

    feat: completed apis for reviews

--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,1 +1,4 @@
 export { UserMovieDto } from './user_movie.dto';
+export { UserRatingDto } from './user_rating.dto';
+export { UserDto } from './user.dto';
+export { UserReviewDto } from './user_review.dto';
