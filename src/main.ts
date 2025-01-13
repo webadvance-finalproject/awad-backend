143f47bbaed3465aae296eba6e8b4f2bcc1103ce
21120537 <145359313+THAQuanttt@users.noreply.github.com>
2025-01-14 00:18:21 +0700

    fix(): Decrease memory

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ dotenv.config();
 
 async function bootstrap() {
   const API_DEFAULT_PORT = 3000;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true ,
+    logger: ['error', 'warn'],
+    bodyParser: true,
+    abortOnError: false
+  });
   await app.listen(process.env.API_PORT || API_DEFAULT_PORT);
 }
 bootstrap();
